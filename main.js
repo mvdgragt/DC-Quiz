9fe65bf214070de6797680e8fad90a1c6e9a85c7
Michiel van der Gragt <michiel@vandergragt.eu>
2021-11-24 21:36:01 +0100

    Points are working for first three questions

--- main.js
+++ main.js
@@ -1,21 +1,49 @@
 // Quiz Questions
 let questions = [
   {
-    question: "Australia is wider than the moon.",
-    answers: [true],
+    question: "Australia is wider than the moon",
+    answers: true,
     correctAnswer: 1,
+    category: "trueOrFalse",
   },
   {
-    question: "Holland is wider than the moon.",
-    answers: [false],
+    question: "Coffee is made from berries",
+    answers: true,
     correctAnswer: 1,
+    category: "trueOrFalse",
   },
   {
-    question: "Sweden has a cross in its flag.",
-    answers: [true],
+    question: "Walt Disney holds the record for the most Oscars",
+    answers: true,
     correctAnswer: 1,
+    category: "trueOrFalse",
   },
+  {
+    question: "How many sides does a square have?",
+    answers: [3,4,5],
+    correctAnswer: 2,
+    category: "chooseBetweenThreeAnswers",
+  },
+  {
+    question: "How many sides does a pentagon have?",
+    answers: [5,6,7],
+    correctAnswer: 1,
+    category: "chooseBetweenThreeAnswers",
+  },
+  {
+    question: "How many sides does a hexagon have?",
+    answers: [5,6,7],
+    correctAnswer: 2,
+    category: "chooseBetweenThreeAnswers",
+  },
+  {
+    question: "Which countries border The Netherlands?",
+    answers: ["Belgium","Germany","France","Denmark"],
+    correctAnswer: [1,2,3],
+    category: "checkboxes",
+  }
 ];
+
 
 // Connecting HTML Elements
 let header = document.querySelector("header");
@@ -33,6 +61,7 @@ let yesButton = document.createElement("button");
 let noButton = document.createElement("button");
 let amountQuestionsAnswered = document.createElement("h2");
 let i = 0;
+let points = 0;
 
 // Setting HTML Attributes
 h1.setAttribute("id", "getReady");
@@ -40,7 +69,9 @@ countdown.setAttribute("id", "countDown");
 question.setAttribute("class", "questions");
 buttonContainer.setAttribute("id", "buttonContainer");
 yesButton.setAttribute("class", "button yesButton");
+yesButton.setAttribute("value", [true]);
 noButton.setAttribute("class", "button noButton");
+noButton.setAttribute("value", false);
 
 // Adding HTML text content
 h1.textContent = "Gör dig redo!";
@@ -61,16 +92,17 @@ let Timer = setInterval(function () {
     });
     // printing first Question below
     question.textContent = questions[i].question;
-    amountQuestionsAnswered.textContent = `Question ${i} / 7 `;
-    main.append(question);
+    amountQuestionsAnswered.textContent = `Question ${i+1} / 7 `;
+    main.append(question,buttonContainer);
     score.append(amountQuestionsAnswered);
-    question.appendChild(buttonContainer);
+    // question.appendChild(buttonContainer);
     buttonContainer.append(yesButton, noButton);
   } else {
     document.querySelector("#countDown").innerHTML = timeleft;
   }
   timeleft -= 1;
 }, 1500);
+
 
 // Toggle
 
@@ -81,26 +113,55 @@ checkbox.addEventListener("change", () => {
   question.classList.toggle("darkText");
 });
 
+// True Button
 yesButton.addEventListener("click", () => {
-   if (i <= 6) {
-    i++;
-    amountQuestionsAnswered.textContent = `Question ${i} / 7 `;
+    if (i<2) {
+      i++;
+    amountQuestionsAnswered.textContent = `Question ${i+1} / 7 `;
     question.textContent = questions[i].question;
     question.appendChild(buttonContainer);
     buttonContainer.append(yesButton, noButton);
-  } else {
-    amountQuestionsAnswered.textContent = `Question 7 / 7 `;
-  }
-});
+    if(yesButton.value && questions[i-1].answers === true) {
+      points++;
+    console.log("amount of correct answers: " + points)
+    console.log("i: " + i)
+        }
+      } else if (i>=2, i<4){
+        yesButton.remove();
+        noButton.remove();
+        i++;
+        amountQuestionsAnswered.textContent = `Question ${i+1} / 7 `;
+        question.textContent = questions[i].question;
+        points++;
+        console.log("amount of correct answers: " + points)
+        console.log("i: " + i)
+           //  amountQuestionsAnswered.textContent = `Question 7 / 7 `;
+       }
+      });
 
+// False Button
 noButton.addEventListener("click", () => {
-  if (i <= 6) {
-   i++;
-   amountQuestionsAnswered.textContent = `Question ${i} / 7 `;
+  if (i<2) {
+    i++;
+   amountQuestionsAnswered.textContent = `Question ${i+1} / 7 `;
    question.textContent = questions[i].question;
    question.appendChild(buttonContainer);
    buttonContainer.append(yesButton, noButton);
- } else {
-   amountQuestionsAnswered.textContent = `Question 7 / 7 `;
+   if(noButton.value && questions[i-1].answers === !true) {
+     points++;
+     console.log("amount of correct answers: " + points)
+     console.log("i: " + i)
+        }
+ } else if (i>=2, i<4){
+  yesButton.remove();
+  noButton.remove();
+  amountQuestionsAnswered.textContent = `Question ${i+1} / 7 `;
+  question.textContent = questions[i].question;
+  points++;
+  console.log("amount of correct answers: " + points)
+  console.log("i: " + i)
+//  amountQuestionsAnswered.textContent = `Question 7 / 7 `;
  }
 });
+
+console.log(questions[0].category);
